2246cfaf6e59a3f64dc28f50800363b2e9973a4b
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-06-25 13:34:33 +0700

    chore: config tailwind color style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        colors: {
+            primary: "#fb2e86",
+            purple: "#7e33e0",
+            "off-purple": "#9f63b5",
+            "navy-blue": "#151875",
+            "navy-blue-off": "#3f509e",
+
+        },
+        extend: {},
+    },
+    plugins: [],
 };
