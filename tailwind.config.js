38a4065071843ad303b059470ffbdf91b8499ea9
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-06-25 17:33:48 +0700

    chore: custom style for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        colors: {
-            primary: "#fb2e86",
-            purple: "#7e33e0",
-            "off-purple": "#9f63b5",
-            "navy-blue": "#151875",
-            "navy-blue-off": "#3f509e",
+const {nextui} = require("@nextui-org/theme");
 
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",],
+    theme: {
+
+        extend: {
+            colors: {
+                primary: "#fb2e86",
+                purple: "#7e33e0",
+                "off-purple": "#9f63b5",
+                "navy-blue": "#151875",
+                "navy-blue-off": "#3f509e",
+                "off-blue": "#151875",
+                "sky-blue": "#f2f5ff",
+                "red": "#fb2448",
+                "blue": "#2f1ac4",
+                "pantone-purple": "#e0d3f5"
+            },
         },
-        extend: {},
     },
-    plugins: [],
+    darkMode: "class",
+    plugins: [nextui()],
+
 };
