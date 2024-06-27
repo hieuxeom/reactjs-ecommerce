cd3649736e0f109367d86c5c6545adb96bc1319d
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-06-27 09:47:40 +0700

    chore: add custom color for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,11 @@ module.exports = {
         extend: {
             colors: {
                 primary: "#fb2e86",
-                purple: "#7e33e0",
                 secondary: "#7e33e0",
+                warning: "#facc15",
+                danger: "#ef4444",
+                success: "#4ade80",
+                purple: "#7e33e0",
                 "sub-background": "#f2f0ff",
                 "off-purple": "#9f63b5",
                 "navy-blue": "#151875",
