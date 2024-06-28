32ea5072d19cd72a136dbcc0dc4727a7e25c12fd
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-06-29 00:19:23 +0700

    feat: create file containing product PropTypes

---
+++ src/utils/propTypes/productType.js
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+
+export const productReviewType = PropTypes.shape({
+    userName: PropTypes.string.isRequired,
+    reviewContent: PropTypes.string.isRequired,
+    reviewStar: PropTypes.number.isRequired
+});
+
+export const productCommentType = PropTypes.shape({
+    userName: PropTypes.string.isRequired,
+    commentContent: PropTypes.string.isRequired
+});
+
+export const productVariantType = PropTypes.shape({
+    variantKey: PropTypes.string.isRequired,
+    variantLabel: PropTypes.string.isRequired,
+    variantImage: PropTypes.string.isRequired,
+    variantStock: PropTypes.number.isRequired,
+    variantPrice: PropTypes.number.isRequired
+});
+
+export const productDetailsType = PropTypes.shape({
+    productName: PropTypes.string.isRequired,
+    productPrice: PropTypes.string.isRequired,
+    isDiscount: PropTypes.bool,
+    discountPercents: PropTypes.number,
+    productCategory: PropTypes.string.isRequired,
+    productReviews: PropTypes.arrayOf(productReviewType),
+    productComments: PropTypes.arrayOf(productCommentType),
+    productRating: PropTypes.number,
+    productVariants: PropTypes.arrayOf(productVariantType),
+    isDeleted: PropTypes.bool,
+    isActive: PropTypes.bool,
+    views: PropTypes.number,
+    soldCount: PropTypes.number
+});
+
