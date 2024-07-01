6324a49c6da90d35d1353e0dd36acdd76fdcb2e8
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-07-01 15:49:31 +0700

    fix: fix maxAge cookie time

--- src/pages/Auth/SignIn/SignIn.jsx
+++ src/pages/Auth/SignIn/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState} from "react";
 import {Button, Input} from "@nextui-org/react";
 import classNames from "classnames";
 import classConfig from "../../../utils/config/class.config.js";
@@ -10,33 +10,34 @@ import {useNavigate} from "react-router-dom";
 function SignIn(props) {
 
     const navigate = useNavigate();
-    const [cookies, setCookie] = useCookies(['refreshToken']);
+    const [cookies, setCookie] = useCookies(["refreshToken"]);
     const axiosClient = useAxios();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleSubmitLogin = () => {
         const credentialData = {
             email,
             password
-        }
+        };
         axiosClient.post(apiUrl.auth.signIn, credentialData).then((response) => {
             const {
                 refreshToken,
-                accessToken,
+                accessToken
             } = response.data.data;
-            setCookie('refreshToken', refreshToken, {path: '/', maxAge: 60 * 24 * 30});
-            setCookie('accessToken', accessToken, {path: '/', maxAge: 5});
+            console.log(60 * 60 * 24 * 30);
+            setCookie("refreshToken", refreshToken, {path: "/", maxAge: 60 * 60 * 24 * 30});
+            setCookie("accessToken", accessToken, {path: "/", maxAge: 5});
         }).finally(() => {
-            navigate('/')
-        })
-    }
+            navigate("/");
+        });
+    };
 
     return (
         <section className={"w-full flex justify-center gap-4 mt-12"}>
             <form className={"min-w-[25vw] border-2 border-black/10 p-8 flex flex-col gap-4 rounded-xl"}>
-                <h3 className={classNames(classConfig.text.h3, classConfig.textVariant.title, 'capitalize')}>Login.</h3>
+                <h3 className={classNames(classConfig.fontSize.h3, classConfig.textVariant.title, "capitalize")}>Login.</h3>
                 <div className={"flex flex-col gap-2"}>
                     <p>Email</p>
                     <Input
