d8ebabe61ddbb3da50a979d73991134bd4b3f01b
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-07-01 15:52:30 +0700

    chore: update something

--- src/pages/Profile/MyProfile.jsx
+++ src/pages/Profile/MyProfile.jsx
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from "react";
 import {apiUrl} from "../../utils/config/api.config.js";
 import useAxiosServer from "../../hooks/useAxiosServer.js";
 
 function MyProfile(props) {
 
     const axiosServer = useAxiosServer();
-    const [userData, setUserData] = useState()
+    const [userData, setUserData] = useState();
     useEffect(() => {
-        axiosServer.get(apiUrl.profile.me).then((response) => {
-            setUserData(response.data)
-        })
+        axiosServer.get(apiUrl.user.me).then((response) => {
+            setUserData(response.data);
+        });
     }, []);
 
     return (
