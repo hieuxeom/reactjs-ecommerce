ffa7cf33d611ecce1d14490a60e227c5f4f28a9f
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-07-02 16:46:42 +0700

    fix: add justifyContent prop for FormRow comp

--- src/components/Form/FormRow.jsx
+++ src/components/Form/FormRow.jsx
@@ -1,15 +1,13 @@
-import React from "react";
 import PropTypes from "prop-types";
 
 FormRow.propTypes = {
-    alignItems: PropTypes.string,
-    children: PropTypes.node
-}
+	alignItems: PropTypes.string,
+	justifyContent: PropTypes.string,
+	children: PropTypes.node,
+};
 
-function FormRow({alignItems = "items-start", children}) {
-    return (
-        <div className={`w-full flex ${alignItems} gap-4`}>{children}</div>
-    );
+function FormRow({ alignItems = "items-start", justifyContent = "justify-start", children }) {
+	return <div className={`w-full flex gap-4  ${alignItems} ${justifyContent}`}>{children}</div>;
 }
 
 export default FormRow;
