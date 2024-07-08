1edbbb13fb5866ff928b5fae95e8ef33280fc0e4
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-07-08 22:40:08 +0700

    chore: column data

---
+++ src/utils/dataDefault/tableColumns.js
@@ -0,0 +1,30 @@
+export const orderTableColumns = [
+    {
+        label: "Mã đơn hàng",
+        key: "order-id"
+    },
+    {
+        label: "Tên khách hàng",
+        key: "customer-name"
+    },
+    {
+        label: "Số lượng sản phẩm",
+        key: "cart-items"
+    },
+    {
+        label: "Giá trị đơn hàng",
+        key: "order-total"
+    },
+    {
+        label: "Trạng thái đơn hàng",
+        key: "order-status"
+    },
+    {
+        label: "Ngày đặt",
+        key: "order-date"
+    },
+    {
+        label: "Hành động",
+        key: "action"
+    }
+];
